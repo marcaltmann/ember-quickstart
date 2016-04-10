4b3a9fe7b5583c98ac53260ac4f93d8fa8ce716b
Marc Altmann <mail@marcaltmann.com>
2016-04-10 21:28:47 +0200

    Dummy commit for Travis CI

--- app/routes/programmers.js
+++ app/routes/programmers.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return ['Marc Altmann', 'Bill Gates', 'Linus Torvalds'];
+    return ['Marc Altmann', 'Bill Gates', 'Linus Torvalds', 'Another guy'];
   }
 });
